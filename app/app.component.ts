212ed4e12af72db9e53514030e14351564df7eff
Anna Becker <anna.r.becker@gmail.com>
2016-03-25 07:48:03 -0700

    Build app.component with Food array.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from 'angular2/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+      <h1>Food Log</h1>
   </div>
     `
 })
 
 export class AppComponent {
-
+  public food: Food[];
+  constructor() {
+    this.food = [
+      new Food("Pizza", "cheese with tomatoes", 400, 0),
+      new Food("Salad", "spinach and strawberry", 150, 1),
+      new Food("Chocolate", "dark chocolate square", 49, 2),
+      new Food("Sandwich", "avocado and turkey", 300, 2),
+      new Food("Eggs", "scrambled", 301, 3)
+    ];
+  }
 }
