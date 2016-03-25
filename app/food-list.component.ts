e71d69bb084381e216df9ac02a12cbcbbc1c79da
Anna Becker <anna.r.becker@gmail.com>
2016-03-25 07:54:17 -0700

    Add food-display selector to food-list.component.

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from 'angular2/core';
 import {Food} from './food.model';
+import {FoodComponent} from "./food.component";
 
 @Component({
   selector: 'food-list',
+  directives: [FoodComponent],
   inputs: ['foodList'],
   template: `
     <div *ngFor="#currentFood of foodList">
@@ -10,6 +12,7 @@ import {Food} from './food.model';
         (click)="foodClicked(currentFood)">&lowast;
         {{ currentFood.name }}
       </h3>
+      <food-display *ngIf="currentFood === selectedFood" [food] = "currentFood"></food-display>
     </div>
   `
 })
