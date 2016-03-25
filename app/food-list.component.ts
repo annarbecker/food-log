adaa8575d837bcb4195fbbdd92209823440a9c79
Anna Becker <anna.r.becker@gmail.com>
2016-03-25 08:07:05 -0700

    Add CaloriesPipe to food-list.component with onChange method to filter food list.

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,15 +1,23 @@
 import { Component } from 'angular2/core';
 import {Food} from './food.model';
-import {FoodComponent} from "./food.component";
-import {NewFoodComponent} from "./new-food.component";
+import {FoodComponent} from './food.component';
+import {NewFoodComponent} from './new-food.component';
+import {CaloriesPipe} from './calories.pipe';
 
 @Component({
   selector: 'food-list',
   directives: [FoodComponent, NewFoodComponent],
+  pipes: [CaloriesPipe],
   inputs: ['foodList'],
   template: `
     <new-food (onSubmitNewFood)="createFood($event)"></new-food>
-    <div *ngFor="#currentFood of foodList">
+    <select (change)="onChange($event.target.value)" class="form-control" id="dropdown">
+      <option value="All Foods" selected="selected">All Foods</option>
+      <option value="Healthy Foods">Healthy Foods</option>
+      <option value="Unhealthy Foods">Unhealthy Foods</option>
+    </select>
+    <hr>
+    <div *ngFor="#currentFood of foodList | calories: filterCalories">
       <h3 class="foodListItem"
         (click)="foodClicked(currentFood)">&lowast;
         {{ currentFood.name }}
@@ -22,6 +30,7 @@ import {NewFoodComponent} from "./new-food.component";
 export class FoodListComponent {
   public foodList: Food[];
   public selectedFood: Food;
+  public filterCalories: string = "All Foods"
   constructor() {}
   foodClicked(clickedFood: Food): void {
     if(this.selectedFood === clickedFood) {
@@ -34,5 +43,8 @@ export class FoodListComponent {
     this.foodList.push (
       new Food(foodArray[0], foodArray[1], foodArray[2], this.foodList.length)
     );
+  }
+  onChange(filterOption) {
+    this.filterCalories = filterOption;
   }
 }
