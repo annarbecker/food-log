1848a160884008e26e36c81bfaada3347c046bad
Anna Becker <anna.r.becker@gmail.com>
2016-03-25 07:56:35 -0700

    Add new-food.component with inputs for user to log new food.

---
+++ app/new-food.component.ts
@@ -0,0 +1,34 @@
+import { Component , EventEmitter} from 'angular2/core';
+import {Food} from './food.model';
+
+@Component({
+  selector: 'new-food',
+  outputs: ['onSubmitNewFood'],
+  template: `
+    <hr>
+      <h4>Add a new food:</h4>
+      <div class="food-form">
+      <input placeholder="Name" class="form-control" #newName>
+      <input placeholder="Description" class="form-control" #newDescription>
+      <input type="number" placeholder="Calories" class="form-control" #newCalories>
+      <button (click)="addFood(newName, newDescription, newCalories)" class="btn btn-default">Add</button>
+      </div>
+    <br>
+  `
+})
+
+export class NewFoodComponent {
+  public onSubmitNewFood: EventEmitter<any>;
+  constructor() {
+    this.onSubmitNewFood = new EventEmitter();
+  }
+  addFood(userName: HTMLInputElement, userDescription: HTMLInputElement, userCalories: HTMLInputElement) {
+    if(parseInt(userCalories.value) >= 0) {
+      var foodArray: Array<any> = [userName.value, userDescription.value, userCalories.value];
+      this.onSubmitNewFood.emit(foodArray);
+      userName.value = "";
+      userDescription.value = "";
+      userCalories.value = "";
+    }
+  }
+}
