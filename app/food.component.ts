7b72dbe683e893b8aa58b3474022eee67e7a66a2
Anna Becker <anna.r.becker@gmail.com>
2016-03-25 08:01:58 -0700

    Add EditFoodComponent directive to food.component to display edit form when a food is clicked on.

--- app/food.component.ts
+++ app/food.component.ts
@@ -1,12 +1,15 @@
 import {Component} from 'angular2/core';
 import {Food} from './food.model';
+import {EditFoodComponent} from './edit-food.component';
 
 @Component({
   selector: 'food-display',
+  directives: [EditFoodComponent],
   inputs: ['food'],
   template: `
     <p>Description: {{food.description}} </p>
     <p>Calories: {{food.calories}} </p>
+    <edit-food [food] = food></edit-food>
   `
 })
 
